e638b36cdc4484651d38a4198e0570e0255290bc
Deon <deonsadcopen@outlook.com>
2024-10-26 14:12:34 +0800

    Add project files.

---
+++ WinFormAppDotNet8SQLite/SignIn.cs
@@ -0,0 +1,15 @@
+namespace WinFormAppDotNet8SQLite
+{
+	public partial class SignIn : Form
+	{
+		public SignIn()
+		{
+			InitializeComponent();
+		}
+
+		private void SignIn_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
